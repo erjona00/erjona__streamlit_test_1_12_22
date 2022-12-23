8436f8d163e4fb35e0ad4c3bd622344fa624f912
erjona00 <119108914+erjona00@users.noreply.github.com>
2022-12-23 19:03:41 +0100

    upload files to repository

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import streamlit as st
 
 #1: Take this input as a string. (hint: you can use st.text_input )
-str = st.text_input("Enter the string: ")
+input_txt = st.text_input("Enter the string: ")
 
 #2: Declare a function called “convert_list”  that takes this string 
 # and returns a list of words from this string.(hint: you can use .split() method ).
 #  Connect this function with a button called “Return List”.
 # When you press this button list of words is printed on streamlit.
 #  (hint: you can use st.write() )
-def convert_list(str):
-    return str.split()
-
-if st.button("Return List"):
-    list = convert_list(str)
+def convert_list(input_txt):
+    str_list = str.split(input_txt)
+    return str_list
+list = convert_list(input_txt)
+list_elements = st.button("Return List")
+if list_elements:
     st.write(list)
 
 #3:  Declare another function called “convert_lower” 
@@ -21,14 +22,14 @@ if st.button("Return List"):
 # Connect this function to a button called “upper” that activates
 #  this function and prints te result.(hint: you need to use st.
 def convert_lower(list):
-    list1 = []
+    listt = []
     for element in list:
-        list2.append(element.upper())
-    return list1
-list1 = convert_lower(list)
-upper_btn = st.button("upper")
-if upper_btn:
-    st.write(list1)
+        listt.append(element.upper())
+    return listt
+listt = convert_lower(list)
+upper_elements = st.button("upper")
+if upper_elements:
+    st.write(listt)
 
 #4: Declare a function called ‘count” that returns the number of elements in the list .
 #Connect this function to a button called “print_count”.
@@ -38,7 +39,7 @@ def count(list):
     for element in list:
         count += 1
     return count
-count_btn = st.button("print_count")
+count_elements = st.button("Print")
 length = count(list)
-if count_btn:
+if count_elements:
     st.write("This list has " + str(length) + " elements.")
